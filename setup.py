e2018f7ae015eb8e6074c8535bed1c40e286ced8
Colin Chan <colinchan@lumeh.org>
2014-06-23 22:46:56 -0700

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='websleydale',
+    version='2.0-alpha1',
+    install_requires=[
+        'Pygments >=1.6',
+        'docutils >=0.11',
+    ],
+    packages=['websleydale'],
+)
