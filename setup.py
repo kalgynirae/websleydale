31f8029f7ca54d516a863fa1a7655018cf82bcfc
Colin Chan <colinchan@lumeh.org>
2014-06-24 22:41:03 -0700

    Remove the dependencies

--- setup.py
+++ setup.py
@@ -3,9 +3,5 @@ from setuptools import setup
 setup(
     name='websleydale',
     version='2.0-alpha1',
-    install_requires=[
-        'Pygments >=1.6',
-        'docutils >=0.11',
-    ],
     packages=['websleydale'],
 )
