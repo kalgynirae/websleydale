b82099fe087c6a203e6e5e8d1ec5ab8175be43bf
Colin Chan <colinchan@lumeh.org>
2020-02-26 22:43:04 -0800

    Rewrite a lot

--- websleydale.py
+++ websleydale.py
@@ -4,13 +4,16 @@ from __future__ import annotations
 import argparse
 import asyncio
 import logging
+import os
 import shutil
 from collections import Counter
 from dataclasses import asdict, dataclass
 from datetime import datetime
+from itertools import chain
 from pathlib import Path
 from shlex import quote
 from subprocess import PIPE
+from tempfile import mkdtemp, mkstemp
 from typing import (
     Any,
     Awaitable,
@@ -37,11 +40,20 @@ jinjaenv = jinja2.Environment(
     lstrip_blocks=True,
     autoescape=True,
 )
+tempdir: Optional[Path] = None
+root = Path(".")
 
 
-@dataclass
-class Result:
-    source_path: Path
+def outdir() -> Path:
+    if tempdir is None:
+        raise RuntimeError("outdir called while tempdir is None")
+    return Path(mkdtemp(dir=str(tempdir)))
+
+
+def outfile() -> Path:
+    if tempdir is None:
+        raise RuntimeError("outdir called while tempdir is None")
+    return Path(mkstemp(dir=str(tempdir))[1])
 
 
 @dataclass
@@ -55,13 +67,25 @@ class Site:
     time: datetime = datetime.now()
 
 
+@dataclass
+class Info:
+    path: str
+    site: Site
+
+
 def build(site: Site, *, dest: str) -> None:
+    global tempdir
+
     destdir = Path(dest)
     if destdir.exists():
         logger.debug("Removing dest directory %s", destdir)
         shutil.rmtree(destdir)
 
+    tempdir = Path(mkdtemp(prefix="websleydale-"))
+    logger.debug("Using tempdir %s", tempdir)
+
     awaitables = []
+    paths = []
     for pathstr, producer in site.tree.items():
         if not pathstr.startswith("/"):
             raise ValueError(f"Invalid path {pathstr!r} (doesn't start with '/')")
@@ -69,16 +93,35 @@ def build(site: Site, *, dest: str) -> None:
             raise TypeError(
                 f"item for path {pathstr!r} has invalid type {type(producer)}"
             )
-        awaitables.append(producer.run(destdir, pathstr, site))
+        dest = destdir / pathstr.lstrip("/")
+        info = Info(path=pathstr, site=site)
+        awaitables.append(copy(dest, producer, info))
+        paths.append(pathstr)
 
     results = asyncio.run(gather(awaitables))
-    for result, path in zip(results, site.tree.keys()):
+    for result, path in zip(results, paths):
         if isinstance(result, Exception):
             logger.error(
                 "[%s] %s: %s", path, result.__class__.__name__, result, exc_info=result
             )
-        else:
+        elif result is None:
             logger.debug("[%s] Success!", path)
+        else:
+            logger.error("[%s] got unexpected result %r", path, result)
+
+
+async def copy(dest: Path, source: FileProducer, info: Info) -> None:
+    source = await source.run(info)
+    if source.path.is_dir():
+        logger.debug("Copying directory %s -> %s", source.path, dest)
+        dest.parent.mkdir(exist_ok=True, parents=True)
+        shutil.copytree(source.path, dest, copy_function=shutil.copy)
+    else:
+        if info.path.endswith("/"):
+            dest = dest / "index.html"
+        logger.debug("Copying file %s -> %s", source.path, dest)
+        dest.parent.mkdir(exist_ok=True, parents=True)
+        shutil.copy(source.path, dest)
 
 
 async def gather(
@@ -87,35 +130,37 @@ async def gather(
     return await asyncio.gather(*awaitables, return_exceptions=True)
 
 
+@dataclass
+class Result:
+    sourceinfo: SourceInfo
+
+
+@dataclass
+class FileResult(Result):
+    path: Path
+
+
+@dataclass
+class TextResult(Result):
+    content: str
+
+
 class FileProducer:
-    async def run(self, destdir: Path, path: str, site: Site) -> Result:
+    async def run(self, info: Info) -> FileResult:
         ...
 
 
-class file(FileProducer):
-    def __init__(self, infile: Path) -> None:
-        self.infile = infile
+class TextProducer:
+    async def run(self, info: Info) -> TextResult:
+        ...
+
+
+class index(FileProducer):
+    def __init__(self, template: str) -> None:
+        self.template = jinjaenv.get_template(template)
 
     async def run(self, destdir: Path, path: str, site: Site) -> Result:
-        dest = destdir / path.lstrip("/")
-        dest.parent.mkdir(exist_ok=True, parents=True)
-        logger.debug("[%s] Copying file", self.infile)
-        shutil.copy(self.infile, dest)
-        return Result(self.infile)
-
-
-class directory(FileProducer):
-    def __init__(self, indir: Path) -> None:
-        self.indir = indir
-
-    async def run(self, destdir: Path, path: str, site: Site) -> Result:
-        dest = destdir / path.lstrip("/")
-        if dest.suffix != "":
-            raise ValueError(f"Expected dest with no extension, got {dest!r}")
-        dest.parent.mkdir(exist_ok=True, parents=True)
-        logger.debug("[%s] Copying directory", self.indir)
-        shutil.copytree(self.indir, dest, copy_function=shutil.copy)
-        return Result(self.indir)
+        ...
 
 
 @dataclass(frozen=True)
@@ -133,18 +178,56 @@ class GitFileInfo:
     updated_date: datetime
 
 
+@dataclass
+class SourceInfo:
+    authors: List[Author]
+    repo_source_path: Path
+    repo_url: str
+    source_path: Path
+    updated_date: datetime
+
+
+class file(FileProducer):
+    def __init__(self, path: Path) -> None:
+        self.path = path
+        if not path.is_file():
+            raise ValueError("not a file: %s", path)
+
+    async def run(self, info: Info) -> FileResult:
+        logger.debug("[%s] Loading Git info", self.path)
+        gitinfo = await _git_file_info(self.path, info.site)
+        sourceinfo = SourceInfo(
+            authors=gitinfo.authors,
+            repo_source_path=gitinfo.repo_source_path,
+            repo_url=gitinfo.repo_url,
+            source_path=self.path,
+            updated_date=gitinfo.updated_date,
+        )
+        return FileResult(sourceinfo=sourceinfo, path=self.path)
+
+
+class dir(FileProducer):
+    def __init__(self, path: Path) -> None:
+        self.path = path
+        if not path.is_dir():
+            raise ValueError("not a dir: %s", path)
+
+    async def run(self, info: Info) -> FileResult:
+        return FileResult(sourceinfo=None, path=self.path)
+
+
 async def _git_file_info(file: Path, site: Site) -> GitFileInfo:
-    stdout, stderr, exitcode = await _run(
-        [
-            "bash",
-            "-c",
-            "--",
-            f"cd {quote(str(file.parent))} && git ls-files --full-name {quote(str(file.name))} && git remote -v",
-        ]
-    )
-    if stderr:
-        for line in stderr.decode(errors="replace").splitlines():
-            logger.debug("[%s] stderr: %s", file, line)
+    args = [
+        "bash",
+        "-c",
+        "--",
+        f"cd {quote(str(file.parent))} && git ls-files --full-name {quote(str(file.name))} && git remote -v",
+    ]
+    proc = await asyncio.create_subprocess_exec(*args, stdout=PIPE)
+    stdout, _ = await proc.communicate()
+    if proc.returncode != 0:
+        raise RuntimeError("git failed")
+
     lines = iter(stdout.decode(errors="replace").splitlines())
     repo_source_path = next(lines)
     repo_url = None
@@ -155,17 +238,16 @@ async def _git_file_info(file: Path, site: Site) -> GitFileInfo:
             break
     if repo_url is not None:
         if repo_url.endswith(".git"):
-            repo_url = repo_url[:-len(".git")]
+            repo_url = repo_url[: -len(".git")]
         if repo_url.startswith("git@github.com:"):
-            repo_name = repo_url[len("git@github.com:"):]
+            repo_name = repo_url[len("git@github.com:") :]
             repo_url = f"https://github.com/{repo_name}"
 
-    stdout, stderr, exitcode = await _run(
-        ["git", "log", "--format=%cI %ae %an", "--", str(file)]
-    )
-    if stderr:
-        for line in stderr.decode(errors="replace").splitlines():
-            logger.debug("[%s] stderr: %s", file, line)
+    args = ["git", "log", "--format=%cI %ae %an", "--", str(file)]
+    proc = await asyncio.create_subprocess_exec(*args, stdout=PIPE)
+    stdout, _ = await proc.communicate()
+    if proc.returncode != 0:
+        raise RuntimeError("git failed")
 
     date = None
     for line in stdout.decode(errors="replace").splitlines():
@@ -178,6 +260,7 @@ async def _git_file_info(file: Path, site: Site) -> GitFileInfo:
             author = Author(display_name=name, email=email, url=None)
         authors[author] = None
     assert date is not None
+
     return GitFileInfo(
         authors=list(authors.keys()),
         repo_source_path=repo_source_path,
@@ -187,86 +270,45 @@ async def _git_file_info(file: Path, site: Site) -> GitFileInfo:
 
 
 class markdown(FileProducer):
-    def __init__(self, infile: Path, template: str) -> None:
-        self.infile = infile
+    def __init__(self, path: Path, template: str) -> None:
+        self.source = file(path)
         self.template = jinjaenv.get_template(template)
 
-    async def run(self, destdir: Path, path: str, site: Site) -> Result:
-        if path.endswith("/"):
-            path += "index.html"
-        elif path.endswith(".html"):
-            pass
-        else:
-            raise ValueError(f"Expected dest ending in '/' or '.html', got {path!r}")
+    async def run(self, info: Info) -> FileResult:
+        source = await self.source.run(info)
+        dest = outfile()
+        content = source.path.read_text()
 
-        dest = destdir / path.lstrip("/")
-        dest.parent.mkdir(exist_ok=True, parents=True)
+        pageinfo = {}
+        if content.startswith("---\n"):
+            yamltext, content = content[4:].split("---\n", maxsplit=1)
+            pageinfo.update(yaml.safe_load(yamltext))
 
-        logger.debug("[%s] Reading source", self.infile)
-        text = self.infile.read_text()
-
-        logger.debug("[%s] Loading Git info", self.infile)
-        gitinfo = await _git_file_info(self.infile, site)
-        pageinfo = {
-            "authors": gitinfo.authors,
-            "path": path,
-            "repo_url": gitinfo.repo_url,
-            "repo_source_path": gitinfo.repo_source_path,
-            "source_path": str(self.infile),
-            "updated_date": gitinfo.updated_date,
-        }
-
-        if text.startswith("---\n"):
-            yamltext, text = text[4:].split("---\n", maxsplit=1)
-            yamlinfo = yaml.safe_load(yamltext)
-            pageinfo.update(yamlinfo)
-
-        logger.debug("[%s] Rendering content", self.infile)
         with HTMLRenderer() as renderer:
-            pageinfo["content"] = renderer.render(Document(text))
+            pageinfo["content"] = renderer.render(Document(content))
 
-        logger.debug("[%s] Rendering template", self.infile)
-        rendered = self.template.render(page=pageinfo, site=site)
-
-        logger.debug("[%s] Writing to %s", self.infile, dest)
+        pageinfo["path"] = info.path
+        rendered = self.template.render(
+            page=pageinfo, site=info.site, source=source.sourceinfo
+        )
         dest.write_text(rendered)
 
-        return Result(self.infile)
+        return FileResult(sourceinfo=source.sourceinfo, path=dest)
 
 
 class sass(FileProducer):
-    def __init__(self, infile: Path) -> None:
-        self.infile = infile
+    def __init__(self, path: Path) -> None:
+        self.source = file(path)
 
-    async def run(self, destdir: Path, path: str, site: Site) -> Result:
-        dest = destdir / path.lstrip("/")
-        if dest.suffix != ".css":
-            raise ValueError(f"Expected dest ending in '.css', got {dest!r}")
-        dest.parent.mkdir(exist_ok=True, parents=True)
-        args = ["pysassc", str(self.infile), str(dest)]
-        await _process_file(self.infile, args)
-        return Result(self.infile)
-
-
-async def _process_file(file: Path, args: List[str]) -> None:
-    logger.debug("[%s] Running command: %s", file, " ".join(quote(arg) for arg in args))
-    stdout, stderr, exitcode = await _run(args)
-    if stdout:
-        for line in stdout.decode(errors="replace").splitlines():
-            logger.debug("[%s] stdout: %s", file, line)
-    if stderr:
-        for line in stderr.decode(errors="replace").splitlines():
-            logger.debug("[%s] stderr: %s", file, line)
-    logger.debug("[%s] Command returned %s", file, exitcode)
-
-
-async def _run(args: List[str]) -> Tuple[bytes, bytes, int]:
-    process = await asyncio.create_subprocess_exec(*args, stderr=PIPE, stdout=PIPE)
-    stdout, stderr = await process.communicate()
-    return stdout, stderr, process.returncode
-
-
-root = Path(".")
+    async def run(self, info: Info) -> FileResult:
+        source = await self.source.run(info)
+        dest = outfile()
+        args = ["pysassc", str(source.path), str(dest)]
+        proc = await asyncio.create_subprocess_exec(*args)
+        exitcode = await proc.wait()
+        if exitcode != 0:
+            raise RuntimeError("pysassc failed")
+        return FileResult(sourceinfo=source.sourceinfo, path=dest)
 
 
 if __name__ == "__main__":
