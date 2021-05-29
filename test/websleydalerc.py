3878193248b7a62bfbd7a572774762758b1a3454
Colin Chan <colinchan@lumeh.org>
2021-05-28 22:08:45 -0700

    Update the test site to use repo/author features

--- test/websleydalerc.py
+++ test/websleydalerc.py
@@ -1,15 +1,26 @@
-from websleydale import Site, build, markdown, root, sass
+from websleydale import (
+    Author, Site, build, jinja, markdown, root, sass
+)
 
 site = Site(
-    name="Test Site",
+    name="Websleydale Test Site",
     tree={
         "test.css": sass(root / "styles/test.sass"),
         **{
-            path.relative_to(root / "pages").with_suffix(".html"): markdown(
-                path, template="page.html"
+            str(path.relative_to(root / "pages").with_suffix(".html")): (
+                jinja(markdown(path), template="page.html")
             )
             for path in root.glob("pages/**/*.md")
         },
+    },
+    repo_name="kalgynirae/websleydale",
+    repo_url="https://github.com/kalgynirae/websleydale",
+    known_authors={
+        Author(
+            display_name="kalgynirae",
+            email="colinchan@lumeh.org",
+            url="https://github.com/kalgynirae/",
+        ),
     },
 )
 build(site, dest="out")
